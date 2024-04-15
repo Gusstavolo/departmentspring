354a91a41758151d86b482d1866489d8a8cfa8b4
Gustavo Oli <69ggustavoo69@gmail.com>
2024-04-15 16:14:40 -0300

    add h2

--- src/main/java/com/gproject/FirstPorject/entities/User.java
+++ src/main/java/com/gproject/FirstPorject/entities/User.java
@@ -1,10 +1,26 @@
 package com.gproject.FirstPorject.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_User")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String name;
 	private String email;
 
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department departmant;
 	
 	public User() {
